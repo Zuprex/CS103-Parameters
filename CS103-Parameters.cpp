a667dc89cfe6ab57cc50ae1744bb9f6369705180
unknown <bermido.charles@gmail.com>
2021-10-11 10:15:57 +1300

    CS103-Parameters

---
+++ CS103-Parameters.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+
+
+int minimumNum(int Array[], int max ) {
+
+    int minValue = 100;
+
+
+    
+
+}
+
+int main()
+{
+
+    const int maxCap = 5;
+    int value[maxCap] = { 1,2,3,4,5 };
+
+    minimumNum(value, maxCap);
+
+}
